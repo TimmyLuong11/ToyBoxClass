86d972152f1f3bd6d6697c1003d27cbaae715195
TimmyLuong <timmyluong11@gmail.com>
2021-04-21 01:06:14 -0500

    Deleted unnecessary code

--- ToyBox/Toy/Program.cs
+++ ToyBox/Toy/Program.cs
@@ -16,50 +16,44 @@ namespace ToyBox
             Toy t1 = new Toy();
             do
             {
-                if (answer == "yes")
+                Console.WriteLine("Please enter a name for a toy box:");
+                person = Console.ReadLine();
+                Console.WriteLine("Please enter a location for a toy box:");
+                area = Console.ReadLine();
+                user = new ToyBox()
                 {
-                    Console.WriteLine("Please enter a name for a toy box:");
-                    person = Console.ReadLine();
-                    Console.WriteLine("Please enter a location for a toy box:");
-                    area = Console.ReadLine();
-                    user = new ToyBox()
-                    {
-                        Owner = person,
-                        Location = area
-                    };
+                    Owner = person,
+                    Location = area
+                };
 
-                }
+
                 do
                 {
-                    if (answer == "yes")
+                    Console.WriteLine("Please enter a name for your toy:");
+                    name = Console.ReadLine();
+                    Console.WriteLine("Please enter a manufacturer for your toy:");
+                    manu = Console.ReadLine();
+                    Console.WriteLine("Please enter a price for your toy:");
+                    amount = Console.ReadLine();
+                    while (double.TryParse(amount, out price) == false)
                     {
-                        Console.WriteLine("Please enter a name for your toy:");
-                        name = Console.ReadLine();
-                        Console.WriteLine("Please enter a manufacturer for your toy:");
-                        manu = Console.ReadLine();
-                        Console.WriteLine("Please enter a price for your toy:");
+                        Console.WriteLine("You did not enter in a number amount! Please try again!");
                         amount = Console.ReadLine();
-                        while (double.TryParse(amount, out price) == false)
-                        {
-                            Console.WriteLine("You did not enter in a number amount! Please try again!");
-                            amount = Console.ReadLine();
-                        }
-                        Console.WriteLine("Please enter any notes for your toy:");
-                        note = Console.ReadLine();
-                        t1 = new Toy()
-                        {
-                            Name = name,
-                            Manufacturer = manu,
-                            Price = price
-                        };
-                        t1.SetNotes(note);
-                        user.Toys.Add(t1);
                     }
+                    Console.WriteLine("Please enter any notes for your toy:");
+                    note = Console.ReadLine();
+                    t1 = new Toy()
+                    {
+                        Name = name,
+                        Manufacturer = manu,
+                        Price = price
+                    };
+                    t1.SetNotes(note);
+                    user.Toys.Add(t1);
                     Console.WriteLine("Would you like to enter in a toy? Type 'yes' or 'no'");
                     answer = Console.ReadLine();
-
-
                 } while (answer != "no");
+
                 Console.WriteLine("Would you like to enter in another person? Type 'yes' or 'no'");
                 answer = Console.ReadLine();
             } while (answer != "no");
